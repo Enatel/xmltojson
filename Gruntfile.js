3a14972945138c530f5142b16add55ebd3b418ee
Drian Naude <drian.naude@enatel.net>
2015-11-11 11:18:35 +1300

    Added formatter and check, some refactoring

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   // require time grunt to measure execution time
   require('time-grunt')(grunt);
@@ -8,7 +8,13 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
       options: {
-        banner: '// <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>\n// v<%= pkg.version %>\n'
+        banner: '/**\n' +
+          ' * <%= pkg.name %> - <%= pkg.description %>\n' +
+          ' * @date <%= grunt.template.today("yyyy/mm/dd") %>\n' +
+          ' * @version <%= pkg.version %>\n' +
+          ' * @link <%= pkg.repository.url %>\n' +
+          ' * @license <%= pkg.license %> License, http://www.opensource.org/licenses/<%= pkg.license %>\n' +
+          ' */\n'
       },
       build: {
         src: 'src/<%= pkg.name %>.js',
@@ -17,23 +23,77 @@ module.exports = function(grunt) {
     },
     bump: {
       options: {
-        files: ['package.json'],
+        files: ['package.json', 'bower.json'],
         updateConfigs: [],
         commit: false,
         createTag: false,
         push: false
       }
     },
+    jsbeautifier: {
+      default: {
+        src: ['Gruntfile.js', 'package.json', '**/*.js', '**/*.less', '**/*.html', '!node_modules/**/*', '!dist/*.js']
+      },
+      verify: {
+        src: ['Gruntfile.js', 'package.json', '**/*.js', '**/*.less', '**/*.html', '!node_modules/**/*', '!dist/*.js'],
+        options: {
+          mode: 'VERIFY_ONLY'
+        }
+      },
+      options: {
+        html: {
+          fileTypes: ['.html'],
+          braceStyle: 'collapse',
+          indentChar: ' ',
+          indentScripts: 'keep',
+          indentSize: 2,
+          maxPreserveNewlines: 1,
+          preserveNewlines: true,
+          unformatted: ['a', 'sub', 'sup', 'b', 'i', 'u'],
+          wrapLineLength: 0
+        },
+        css: {
+          fileTypes: ['.less'],
+          indentChar: ' ',
+          indentSize: 2
+        },
+        js: {
+          fileTypes: ['.js'],
+          braceStyle: 'collapse',
+          breakChainedMethods: false,
+          e4x: false,
+          evalCode: false,
+          indentChar: ' ',
+          indentLevel: 0,
+          indentSize: 2,
+          indentWithTabs: false,
+          jslintHappy: true,
+          keepArrayIndentation: false,
+          keepFunctionIndentation: false,
+          maxPreserveNewlines: 2,
+          preserveNewlines: true,
+          spaceBeforeConditional: true,
+          spaceInParen: false,
+          unescapeStrings: false,
+          wrapLineLength: 0,
+          endWithNewline: true
+        }
+      }
+
+    }
   });
 
   // Load grunt Tasks
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-bump');
+  grunt.loadNpmTasks('grunt-jsbeautifier');
 
   // Default task(s).
   grunt.registerTask('minify', ['uglify']);
-  grunt.registerTask('patch', ['bump:patch', 'uglify']);
-  grunt.registerTask('minor', ['bump:minor', 'uglify']);
-  grunt.registerTask('major', ['bump:major', 'uglify']);
+  grunt.registerTask('verify', ['jsbeautifier:verify']);
+  grunt.registerTask('format', ['jsbeautifier:default']);
+  grunt.registerTask('patch', ['jsbeautifier:verify', 'bump:patch', 'uglify']);
+  grunt.registerTask('minor', ['jsbeautifier:verify', 'bump:minor', 'uglify']);
+  grunt.registerTask('major', ['jsbeautifier:verify', 'bump:major', 'uglify']);
 
 };
